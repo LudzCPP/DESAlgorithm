db47bd145cd05dd7f12f97e30df20e7ecb119799
Kacper Janowicz 242407 <242407@edu.p.lodz.pl>
2023-04-15 18:38:22 +0200

    Idzie dobrze

--- DESAlgorithm/Program.cs
+++ DESAlgorithm/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 using System.Text;
 
 namespace DESAlgorithm
 {
     class Program
     {
-        private byte[] KeyPermutPC1 = new byte[]{
+        private static int[] KeyPermutPC1 = new int[]{
             57, 49, 41, 33, 25, 17, 9,
             1, 58, 50, 42, 34, 26, 18,
             10, 2, 59, 51, 43, 35, 27,
@@ -15,10 +17,10 @@ namespace DESAlgorithm
             14, 6, 61, 53, 45, 37, 29,
             21, 13, 5, 28, 20, 12, 4};
 
-        private byte[] KeyShifts = new byte[]
+        private static int[] KeyShifts = new int[]
                 {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
 
-        private byte[] KeyPermutPC2 = new byte[]{
+        private static int[] KeyPermutPC2 = new int[]{
             14, 17, 11, 24, 1, 5,
             3, 28, 15, 6, 21, 10,
             23, 19, 12, 4, 26, 8,
@@ -28,7 +30,7 @@ namespace DESAlgorithm
             44, 49, 39, 56, 34, 53,
             46, 42, 50, 36, 29, 32};
 
-        private byte[] InitPermut = new byte[]{
+        private int[] InitPermut = new int[]{
             58, 50, 42, 34, 26, 18, 10, 2,
             60, 52, 44, 36, 28, 20, 12, 4,
             62, 54, 46, 38, 30, 22, 14, 6,
@@ -38,7 +40,7 @@ namespace DESAlgorithm
             61, 53, 45, 37, 29, 21, 13, 5,
             63, 55, 47, 39, 31, 23, 15, 7};
 
-        private byte[] ESelectionTable = new byte[]{
+        private int[] ESelectionTable = new int[]{
             32, 1, 2, 3, 4, 5,
             4, 5, 6, 7, 8, 9,
             8, 9, 10, 11, 12, 13,
@@ -48,49 +50,49 @@ namespace DESAlgorithm
             24, 25, 26, 27, 28, 29,
             28, 29, 30, 31, 32, 1};
 
-        private byte[][] SBoxes = new byte[][]{
-            new byte[]{
+        private int[][] SBoxes = new int[][]{
+            new int[]{
                     14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
                     0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
                     4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
                     15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13},
-            new byte[]{
+            new int[]{
                     15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
                     3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
                     0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15,
                     13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9},
-            new byte[]{
+            new int[]{
                     10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
                     13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
                     13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7,
                     1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12},
-            new byte[]{
+            new int[]{
                     7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
                     13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
                     10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
                     3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14},
-            new byte[]{
+            new int[]{
                     2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
                     14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6,
                     4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14,
                     11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3},
-            new byte[]{
+            new int[]{
                     12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
                     10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8,
                     9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6,
                     4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13},
-            new byte[]{
+            new int[]{
                     4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
                     13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6,
                     1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2,
                     6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12},
-            new byte[]{
+            new int[]{
                     13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
                     1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
                     7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
                     2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11}};
 
-        private byte[] PBlockPermut = new byte[]{
+        private int[] PBlockPermut = new int[]{
             16, 7, 20, 21,
             29, 12, 28, 17,
             1, 15, 23, 26,
@@ -100,7 +102,7 @@ namespace DESAlgorithm
             19, 13, 30, 6,
             22, 11, 4, 25};
 
-        private byte[] ReversedInitialPermut = new byte[]{
+        private int[] ReversedInitialPermut = new int[]{
             40, 8, 48, 16, 56, 24, 64, 32,
             39, 7, 47, 15, 55, 23, 63, 31,
             38, 6, 46, 14, 54, 22, 62, 30,
@@ -110,30 +112,30 @@ namespace DESAlgorithm
             34, 2, 42, 10, 50, 18, 58, 26,
             33, 1, 41, 9, 49, 17, 57, 25};
 
-        public static int ByteArrayToInt(byte[] byteArray)
+        public static int intArrayToInt(int[] intArray)
         {
             int result = 0;
-            for (int i = byteArray.Length - 1; i >= 0; i--)
+            for (int i = intArray.Length - 1; i >= 0; i--)
             {
-                result += byteArray[i] * (int)Math.Pow(2, byteArray.Length - 1 - i);
+                result += intArray[i] * (int)Math.Pow(2, intArray.Length - 1 - i);
             }
             return result;
         }
 
-        public static byte[] IntToByteArray(int Value, int NumOfBytes)
+        public static int[] IntTointArray(int Value, int NumOfints)
         {
-            byte[] byteArray = new byte[NumOfBytes]; // 32 bajty dla liczby 32-bitowej
-            for (int i = byteArray.Length - 1; i >= 0; i--)
+            int[] intArray = new int[NumOfints]; // 32 bajty dla liczby 32-bitowej
+            for (int i = intArray.Length - 1; i >= 0; i--)
             {
-                byteArray[i] = (byte)(Value & 0x01);
+                intArray[i] = (int)(Value & 0x01);
                 Value >>= 1;
             }
-            return byteArray;
+            return intArray;
         }
 
-        private byte[] Permut(byte[] Input, byte[] PermutTab)
+        private static int[] Permut(int[] Input, int[] PermutTab)
         {
-            byte[] Result = new byte[PermutTab.Length];
+            int[] Result = new int[PermutTab.Length];
 
             for (int i = 0; i < PermutTab.Length; i++)
             {
@@ -143,22 +145,127 @@ namespace DESAlgorithm
             return Result;
         }
 
+        public static int[][] StringToBinaryTable(string inputString)
+        {
+          
 
+            int[][] binaryTable = new int[inputString.Length][];
 
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                int asciiValue = inputString[i];
+                string binaryString = Convert.ToString(asciiValue, 2).PadLeft(8, '0');
+                binaryTable[i] = new int[binaryString.Length];
+                for (int j = 0; j < binaryString.Length; j++)
+                {
+                    binaryTable[i][j] = binaryString[j] - '0';
+                }
+            }
+
+            return binaryTable;
+        }
+
+        public static void Show2DTable(int[][] binaryTable)
+        {
+            for (int i = 0; i < binaryTable.Length; i++)
+            {
+                for (int j = 0; j < binaryTable[i].Length; j++)
+                {
+                    Console.Write(binaryTable[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void ShowTable(int[] table)
+        {
+            foreach(int i in table)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public static int[] PadAndFlattenBinaryTable(int[][] binaryTable)
+        {
+            int maxRows = 8;
+            int[] flattenedTable = new int[maxRows * 8];
+
+            for (int i = 0; i < maxRows; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (i < binaryTable.Length && j < binaryTable[i].Length)
+                    {
+                        flattenedTable[i * 8 + j] = binaryTable[i][j];
+                    }
+                    else
+                    {
+                        flattenedTable[i * 8 + j] = 0;
+                    }
+                }
+            }
+
+            return flattenedTable;
+        }
+
+        public static (int[], int[]) ShiftLeft(int[] inputC, int[] inputD, int shiftCount)
+        {
+            int length1 = inputC.Length;
+            int length2 = inputD.Length;
+            int[] shiftedArray1 = new int[length1];
+            int[] shiftedArray2 = new int[length2];
+
+            for (int i = 0; i < length1; i++)
+            {
+                int newIndex = (i - shiftCount + length1) % length1;
+                shiftedArray1[newIndex] = inputC[i];
+            }
+
+            for (int i = 0; i < length2; i++)
+            {
+                int newIndex = (i - shiftCount + length2) % length2;
+                shiftedArray2[newIndex] = inputD[i];
+            }
+
+            //ShowTable(shiftedArray);
+            return (shiftedArray1, shiftedArray2);
+        }
 
 
 
         static void Main(string[] args)
         {
-            String message = "0123456789ABCDEF";
-            String key = "KLUCZ";
-            /*foreach(byte i in tab)
+            String message = "0123";
+            String key = "zzzzzzzz";
+
+            int[] xd = new int[]{0,0,0,1,0,0,1,1,0,0,1,1,0,1,0,0,0,1,0,1,0,1,1,1,0,1,1,1,1,0,0,1,1,0,0,1,1,0,1,1,
+                1,0,1,1,1,1,0,0,1,1,0,1,1,1,1,1,1,1,1,1,0,0,0,1 };
+
+            int[][] Keys = new int[16][];
+
+            int[] xd1 = Permut(xd, KeyPermutPC1);
+            int[] C = xd1.Take(xd1.Length / 2).ToArray();
+            int[] D = xd1.Skip(xd1.Length / 2).ToArray();
+           
+
+            for (int i = 0; i < 16; i++)
             {
-                Console.WriteLine(i);
-            }*/
+                (C, D) = ShiftLeft(C, D, KeyShifts[i]);
+                Keys[i] = Permut(C.Concat(D).ToArray(), KeyPermutPC2);
+            }
+
 
             
-            
+
+            //ShowTable(ShiftedArrayC.Concat(ShiftedArrayD).ToArray());
+            //ShowTable(ShiftedArrayD);
+
+            //Console.WriteLine();
+            for (int i = 0; i < 16; i++)
+            {
+                ShowTable(Keys[i]);
+            } 
         }
     }
 }
